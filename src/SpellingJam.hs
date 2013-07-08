d9c13b7d5033ed6284276549725d7b8a6f1d3161
Cole Brown <bigswim@gmail.com>
2013-07-08 13:45:01 -0500

    Define type signatures

--- src/SpellingJam.hs
+++ src/SpellingJam.hs
@@ -1,6 +1,32 @@
 module Main where
 
+import Data.List (words)
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+type FrequencyMap = M.Map String Integer
+type Word = String
+type Edit = String
+type Edits = S.Set Edit
+
+train :: [Word] -> FrequencyMap
+
+nWords :: Word -> FrequencyMap
+
+alphabet :: [Char]
+alphabet = ['a'..'z']
+
+edits1 :: Word -> Edits
+
+knownEdits2 :: Word -> Edits
+
+known :: Word -> Edits
+
+correct :: Word -> Edit
+
 main :: IO ()
 main = do
-    putStrLn "SPELLING JAM"
+    putStr "word> "
+    word <- getLine
+    putStrLn . correct $ word
 
